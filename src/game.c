e4ea98ed41be5d739cb282506b254cd94111b4f5
P. Pavlov <kx355@mail.ru>
2025-01-15 18:54:33 +0300

    implement game_init()

---
+++ src/game.c
@@ -0,0 +1,29 @@
+#include "../include/game.h"
+#include "SDL3/SDL_log.h"
+
+bool game_init(Game *game) {
+
+  if (!(SDL_Init(SDL_INIT_VIDEO))) {
+    SDL_Log("SDL initialized failed %s", SDL_GetError());
+    return false;
+  } else if (!(game->window = SDL_CreateWindow("snake", screen_width,
+                                               screen_height, NULL))) {
+    SDL_Log("SDL Window creation failed %s", SDL_GetError());
+    SDL_Quit();
+    return false;
+  }
+  if (!(game->renderer = SDL_CreateRenderer(game->window, 0))) {
+    SDL_Log("SDL Renderer creation failed %s", SDL_GetError());
+    SDL_DestroyWindow(game->window);
+    SDL_Quit();
+    return false;
+  }
+
+  Snake snake = {{screen_width / 2, screen_height / 2}, NULL, Right};
+  Fruit fruit = {0, 0};
+
+  game->snake = snake;
+  game->fruit = fruit;
+  game->score = 0;
+  game->running = true;
+}
