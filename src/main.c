053b4f63e50163b8d7039a185e2aa17b4f9e70fe
Pavel Pavlov <kx355@mail.ru>
2025-01-15 21:56:43 +0300

    remove stdio.h include

--- src/main.c
+++ src/main.c
@@ -1,13 +1,13 @@
 #include "../include/game.h"
-#include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[])
+{
 
-  Game game;
-  game_init(&game);
-  /* run_game(&game); */
-  quit_game(&game);
+    Game game;
+    game_init(&game);
+    /* run_game(&game); */
+    quit_game(&game);
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
