c651c38c33e52b3acd897e3776ac87930f109821
P. Pavlov <kx355@mail.ru>
2025-01-16 18:25:13 +0300

    now snake reset position after delay when out

--- src/logic.c
+++ src/logic.c
@@ -1,6 +1,20 @@
 #include "../include/logic.h"
 #include "SDL3/SDL_events.h"
 
+void reset_snake(Snake *snake) {
+  SDL_Delay(1000);
+  snake->head.x = screen_width / 2;
+  snake->head.y = screen_height / 2;
+}
+
+bool snake_out_border(Snake *snake) {
+  if (snake->head.x < 0 || snake->head.x + grid_size > screen_width ||
+      snake->head.y < 0 || snake->head.y + grid_size > screen_height) {
+    return true;
+  }
+  return false;
+}
+
 void input_handle(Game *game, SDL_Event *event) {
   while (SDL_PollEvent(event) != 0) {
     if (event->type == SDL_EVENT_QUIT) {
@@ -49,5 +63,9 @@ void update(Game *game) {
   case Right:
     game->snake.head.x += grid_size;
     break;
+  }
+
+  if (snake_out_border(&game->snake)) {
+    reset_snake(&game->snake);
   }
 }
