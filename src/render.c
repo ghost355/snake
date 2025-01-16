97a62b391e63c4741cab02e36633eb068a67900c
Pavel Pavlov <kx355@mail.ru>
2025-01-16 12:26:16 +0300

    impement render_snake()

--- src/render.c
+++ src/render.c
@@ -1,15 +1,28 @@
 #include "../include/render.h"
 
-void render_snake(Snake* snake);
-void render_fruit(Fruit* fruit);
+void render_snake(Game* game);
+void render_fruit(Game* game);
 
 void render(Game* game)
 {
     SDL_SetRenderDrawColor(game->renderer, 0, 0, 0, 255);
 
     SDL_RenderClear(game->renderer);
-    render_snake(&game->snake);
-    render_fruit(&game->fruit);
+    render_snake(game);
+    render_fruit(game);
 
     SDL_RenderPresent(game->renderer);
+}
+
+void render_snake(Game* game)
+{
+    SDL_SetRenderDrawColor(game->renderer, 255, 255, 255, 255);
+    SDL_FRect head_rect
+      = { game->snake.head.x, game->snake.head.y, grid_size, grid_size };
+    SDL_RenderFillRect(game->renderer, &head_rect);
+    Point* body = game->snake.body;
+    for (int i = 0; i < game->snake.lenght; i++) {
+        SDL_FRect body_rect = { body[i].x, body[i].y, grid_size, grid_size };
+        SDL_RenderFillRect(game->renderer, &body_rect);
+    }
 }
