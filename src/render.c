ec02428f0222a523d8a50bfce8c00ce720b0544c
P. Pavlov <kx355@mail.ru>
2025-01-17 14:49:35 +0300

    fix tail showing issue after death

--- src/render.c
+++ src/render.c
@@ -1,37 +1,33 @@
 #include "../include/render.h"
 
-void render_snake(Game* game);
-void render_fruit(Game* game);
+void render_snake(Game *game);
+void render_fruit(Game *game);
 
-void render(Game* game)
-{
-    SDL_SetRenderDrawColor(game->renderer, 0, 0, 0, 255);
-    SDL_RenderClear(game->renderer);
+void render(Game *game) {
+  SDL_SetRenderDrawColor(game->renderer, 0, 0, 0, 255);
+  SDL_RenderClear(game->renderer);
 
-    render_snake(game);
-    render_fruit(game);
+  render_snake(game);
+  render_fruit(game);
 
-    SDL_RenderPresent(game->renderer);
-    SDL_Delay(1000 / 15);
+  SDL_RenderPresent(game->renderer);
+  SDL_Delay(1000 / 15);
 }
 
-void render_snake(Game* game)
-{
-    SDL_SetRenderDrawColor(game->renderer, 255, 255, 255, 255);
-    SDL_FRect head_rect
-      = { game->snake.head.x, game->snake.head.y, grid_size, grid_size };
-    SDL_RenderFillRect(game->renderer, &head_rect);
-    for (int i = 0; i < game->snake.length; i++) {
-        SDL_FRect body_rect = { game->snake.body[i].x, game->snake.body[i].y,
-                                grid_size, grid_size };
-        SDL_RenderFillRect(game->renderer, &body_rect);
-    }
+void render_snake(Game *game) {
+  SDL_SetRenderDrawColor(game->renderer, 255, 255, 255, 255);
+  SDL_FRect head_rect = {game->snake.head.x, game->snake.head.y, grid_size,
+                         grid_size};
+  SDL_RenderFillRect(game->renderer, &head_rect);
+  for (int i = 0; i < game->snake.length; i++) {
+    SDL_FRect body_rect = {game->snake.body[i].x, game->snake.body[i].y,
+                           grid_size, grid_size};
+    SDL_RenderFillRect(game->renderer, &body_rect);
+  }
 }
 
-void render_fruit(Game* game)
-{
-    SDL_SetRenderDrawColor(game->renderer, 0, 255, 0, 255);
-    SDL_FRect fruit_rect
-      = { game->fruit.x, game->fruit.y, grid_size + 6, grid_size + 6 };
-    SDL_RenderFillRect(game->renderer, &fruit_rect);
+void render_fruit(Game *game) {
+  SDL_SetRenderDrawColor(game->renderer, 0, 255, 0, 255);
+  SDL_FRect fruit_rect = {game->fruit.x, game->fruit.y, grid_size, grid_size};
+  SDL_RenderFillRect(game->renderer, &fruit_rect);
 }
