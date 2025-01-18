5291e43714ba37773113211e734caba6980e740e
Pavel Pavlov <kx355@mail.ru>
2025-01-18 18:40:09 +0300

    Add sturct Text for labels

--- include/game.h
+++ include/game.h
@@ -1,6 +1,7 @@
 #ifndef GAME_H
 #define GAME_H
 #include <SDL3/SDL.h>
+#include <SDL3_ttf/SDL_ttf.h>
 
 #define screen_width 800
 #define screen_height 600
@@ -8,34 +9,51 @@
 #define initial_len 2
 #define max_lenght 100
 
-typedef struct Point {
-  int x, y;
+typedef struct Point
+{
+    int x, y;
 } Point;
 
-typedef enum Direction { Up, Down, Left, Right } Direction;
+typedef enum Direction
+{
+    Up,
+    Down,
+    Left,
+    Right
+} Direction;
 
-typedef struct Snake {
-  Point head;
-  Point body[max_lenght];
-  Direction direction;
-  int length;
+typedef struct Snake
+{
+    Point     head;
+    Point     body[max_lenght];
+    Direction direction;
+    int       length;
 } Snake;
 
-typedef struct Game {
-  SDL_Window *window;
-  SDL_Renderer *renderer;
-  Snake snake;
-  Point fruit;
-  int score;
-  bool running;
-  bool fruit_eaten;
-  // TODO add State to Game and enum for States
+typedef struct Text
+{
+    char score_label[32];
+    char length_label[32];
+} Text;
+
+typedef struct Game
+{
+    SDL_Window*   window;
+    SDL_Renderer* renderer;
+    TTF_Font*     font;
+    Snake         snake;
+    Point         fruit;
+    int           score;
+    bool          running;
+    bool          fruit_eaten;
+    Text*         label;
+    // TODO add State to Game and enum for States
 } Game;
 
-bool game_init(Game *game);
+bool game_init(Game* game);
 
-void run_game(Game *game);
+void run_game(Game* game);
 
-void quit_game(Game *game);
+void quit_game(Game* game);
 
 #endif
